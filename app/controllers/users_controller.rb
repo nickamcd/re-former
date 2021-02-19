1644635b9a371d4676fd27317c97d499a4ce49e9
Nick McDaniel <nickamcd@gmail.com>
2021-02-19 13:10:06 -0800

    Create user action built

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+  
+    if @user.save
+      redirect_to new_user_path
+    else
+      puts @user.error.full_messages
+      render :new
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
